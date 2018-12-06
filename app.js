10cafe1674c4f4a3c4b230df61d4258724e6cc1b
LEEILHO <dldlfgh2642@naver.com>
2018-12-06 19:52:14 +0900

    commit

--- app.js
+++ app.js
@@ -31,9 +31,10 @@ app.locals.querystring = require('querystring');
 // mongodb connect
 //=======================================================
 mongoose.Promise = global.Promise; // ES6 Native Promise를 mongoose에서 사용한다.
-const connStr = 'mongodb://localhost/mjdb1';
 // 아래는 mLab을 사용하는 경우의 예: 본인의 접속 String으로 바꾸세요.
-// const connStr = 'mongodb://dbuser1:mju12345@ds113825.mlab.com:13825/sampledb1';
+const connStr = (process.env.NODE_ENV == 'production')?
+ 'mongodb://dbuser1:20dlfalsjc@ds229373.mlab.com:29373/project' : 
+ 'mongodb://localhost/mjdb2';
 mongoose.connect(connStr, {useMongoClient: true });
 mongoose.connection.on('error', console.error);
 
